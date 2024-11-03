71c33e5c1482db8cf72e88a9dfbabac3209be79f
Alex Roel <56522186+alexroel@users.noreply.github.com>
2024-11-03 11:12:02 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://roelcode.github.io',
+	site: 'https://roelcode.com',
 	integrations: [mdx(), sitemap()],
 });
