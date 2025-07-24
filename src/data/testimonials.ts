5aa8a68452cbfb2f4ec6354607d72c03c8e96598
alexroel <alexroel@example.com>
2025-07-23 19:16:45 -0500

    reolcode-v5.0.1

---
+++ src/data/testimonials.ts
@@ -0,0 +1,42 @@
+export const testimonials = [
+    {
+        name: "Donovan Abraham",
+        description: "Ha sido un gran curso, muy dinámico y práctico.",
+        calification: 5,
+    },
+    {
+        name: "Alexander Guillin",
+        description: "Excelente curso, la forma sencilla de explicar permite comprender mejor cada tema.",
+        calification: 5,
+    },
+    {
+        name: "Gilberto Gaytan",
+        description: "Curso 100% recomendado, cumplio con todas mis expectativas!",
+        calification: 5,
+    },
+    {
+        name: "Gina paola",
+        description: "Excelente curso, muy bien explicado, fácil de entender, muy claro. Muchas gracias.",
+        calification: 5,
+    },
+    {
+        name: "Eduardo Andres",
+        description: "En la mayor parte del curso me he encontrado con información desactualizada sobre el lenguaje o incluso sobre las bases de datos, es algo que se debe mejorar.",
+        calification: 5,
+    },
+    {
+        name: "Edison Alberto",
+        description: "La información es transmitida de una manera clara y concisa. ¡Además es muy dinámico y divertido para explicar!",
+        calification: 5,
+    },
+     {
+         name: "Carlos Mario",
+         description: "Llevo poco más de 2 semanas con el curso y he aprendido lo que no aprendí en 2 semestres de la universidad respecto a HTML y CSS.",
+         calification: 5,
+     },
+    {
+         name: "Yazmin Salazar",
+         description: "Está muy bien explicado, dicen que es Java e indican qué aplicaciones se usan para poder programarlo. Muy buen timbre de voz y entonación.",
+         calification: 5,
+    }
+];
